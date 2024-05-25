6d46f00b147d2358202016d23a40616f00dca7ab
Stalkmind1256 <sterhov.andrej2016@yandex.ru>
2024-05-25 15:30:27 +0300

    add find pixel

--- RoadToFirstPlace/Camera.py
+++ RoadToFirstPlace/Camera.py
@@ -1,13 +1,12 @@
 import cv2
+import numpy as np
 import threading
 
 class RTSPCamera:
     def __init__(self, rtsp_url):
         self.rtsp_url = rtsp_url
-
         self.cap = cv2.VideoCapture(self.rtsp_url)
         self.frame = None
-
         self.running = False
         self.thread = None
 
@@ -20,7 +19,6 @@ class RTSPCamera:
         """Поток получения кадров"""
         while self.running:
             ret, frame = self.cap.read()
-
             if ret:
                 self.frame = frame
             else:
@@ -36,3 +34,17 @@ class RTSPCamera:
 
     def __del__(self):
         self.stop()
+
+    def find_pixels(self, target_color_bgr, delta=10):
+        if self.frame is None:
+            return None, None
+        # Конвертация из BGR в HSV
+        target_color_hsv = cv2.cvtColor(np.uint8([[target_color_bgr]]), cv2.COLOR_BGR2HSV)[0][0]
+        lower_bound = np.array([target_color_hsv[0]-delta, 100, 100])
+        upper_bound = np.array([target_color_hsv[0]+delta, 255, 255])
+
+        hsv_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv_frame, lower_bound, upper_bound)
+        result_frame = cv2.bitwise_and(self.frame, self.frame, mask=mask)
+
+        return result_frame, mask
--- RoadToFirstPlace/main.py
+++ RoadToFirstPlace/main.py
@@ -1,5 +1,9 @@
 import cv2
+import numpy
 from Camera import RTSPCamera
+
+#target_color = [152, 255, 152] # Цвет в формате BGR
+target_color = [229, 244, 255]
 
 camera = RTSPCamera('http://158.58.130.148:80/mjpg/video.mjpg')
 camera.start()
@@ -9,7 +13,10 @@ while True:
     if frame is None:
         continue
 
+    result_frame, mask = camera.find_pixels(target_color)
     cv2.imshow('frame', frame)
+    cv2.imshow('result', result_frame)
+    cv2.imshow('mask', mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
